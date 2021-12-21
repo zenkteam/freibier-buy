6a536b8cf3175b16b941d44f797ca1a649b33835
Max Klenk <max@klenk.biz>
2021-12-21 10:31:45 +0100

    update rpc

--- src/config.ts
+++ src/config.ts
@@ -23,7 +23,7 @@ const config = {
 
 switch (config.network) {
   case NetworkType.MAINNET:
-    config.rpcUrl = "https://mainnet.smartpy.io/";
+    config.rpcUrl = "https://rpc.tzbeta.net/"; // "https://mainnet.smartpy.io/";
     break;
   case NetworkType.FLORENCENET:
     config.rpcUrl = "https://rpc.florence.tzstats.com/";
